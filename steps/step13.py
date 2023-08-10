e2da83ad1348055f1c5faa95adda7287a4d9a764
PILSUCHOI <chlvlftn22@naver.com>
2023-08-10 14:51:24 +0900

    step13 add

---
+++ steps/step13.py
@@ -0,0 +1,98 @@
+import numpy as np
+
+# 가변 길이 인수(역전파 편)
+# Variable class 수정
+# Square class 수정
+
+
+class Variable:
+    def __init__(self, data):
+        if data is not None:  # ndarray만 취급하기
+            if not isinstance(data, np.ndarray):
+                raise TypeError('{}은(는) 지원하지 않습니다.', format(type(data)))
+
+        self.data = data
+        self.grad = None
+        self.creator = None
+
+    def set_creator(self, func):
+        self.creator = func
+
+    def backward(self):
+        if self.grad is None:
+            self.grad = np.ones_like(self.data)
+
+        funcs = [self.creator]
+        while funcs:
+            f = funcs.pop()
+            gys = [output.grad for output in f.ouputs]
+            gxs = f.backward(*gys)
+            if not isinstance(gxs, tuple):
+                gxs = (gxs,)
+
+            for x, gx in zip(f.inputs, gxs):
+                x.grad = gx
+
+            if x.creator is not None:
+                funcs.append(x.creator)  # 하나 앞의 함수를 리스트에 추가한다
+
+
+def as_array(x):
+    if np.isscalar(x):
+        return np.array(x)
+    return x
+
+
+class Function:
+    def __call__(self, input):
+        x = input.data
+        y = self.forward(x)  # 구체적인 계산은 forward 메서드에서 한다
+        output = Variable(as_array(y))
+        output.set_creator(self)
+        self.input = input  # 입력 변수를 기억(보관)한다
+        self.output = output
+        return output
+
+    def forward(self, x):
+        raise NotImplementedError()
+
+    def backward(self, gy):
+        raise NotImplementedError()
+
+
+class Square(Function):
+    def forward(self, x):
+        return x ** 2
+
+    def backward(self, gy):
+        x = self.inputs[0].data
+        gx = 2 * x * gy
+        return gx
+
+
+def square(x):
+    f = Square()
+    return f(x)
+
+
+class Add(Function):
+    def forward(self, x0, x1):
+        y = x0 + x1
+        return y
+
+
+def add(x0, x1):
+    return Add()(x0, x1)
+
+
+## example test codes ##
+'''
+x = Variable(np.array(2.0))
+y = Variable(np.array(3.0))
+
+z = add(square(x), square(y))
+z.backward()
+print(z.data)
+print(x.grad)
+print(y.grad)
+'''
