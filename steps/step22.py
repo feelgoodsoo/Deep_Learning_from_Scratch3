4d4b47ed0193b25f54d9cb0aec68474aeb2117ef
PILSUCHOI <chlvlftn22@gmail.com>
2023-08-11 10:06:27 +0900

    ~step23 add

---
+++ steps/step22.py
@@ -0,0 +1,295 @@
+import numpy as np
+import weakref
+import contextlib
+
+## 연산자 오버로드(3) ##
+# 아래 메서드 추가
+# __neg__(self)  -> 양수를 음수로, 음수를 양수로 ( -self )
+# __sub__(self, other) -> 뺄셈 ( self - other )
+# __rsub__(self, other) -> 뺄셈 ( other - self )
+# __truediv__(self, other) -> 나눗셈 ( other/self )
+# __rtruediv__(self, other) -> 나눗셈 ( self/other )
+# __pow__ -> 거듭제곱 ( self ** other )
+
+
+class Variable:
+    def __init__(self, data, name=None):
+        if data is not None:  # ndarray만 취급하기
+            if not isinstance(data, np.ndarray):
+                raise TypeError('{}은(는) 지원하지 않습니다.', format(type(data)))
+
+        self.data = data
+        self.name = name
+        self.grad = None
+        self.creator = None
+        self.generation = 0  # 세대 수를 기록하는 변수
+
+    def set_creator(self, func):
+        self.creator = func
+        self.generation = func.generation + 1  # 세대를 기록한다(부모 세대 + 1 )
+
+    def backward(self, retain_grad=False):
+        if self.grad is None:
+            self.grad = np.ones_like(self.data)
+
+        funcs = []
+        seen_set = set()
+
+        def add_func(f):
+            if f not in seen_set:
+                funcs.append(f)
+                seen_set.add(f)
+                funcs.sort(key=lambda x: x.generation)
+
+        add_func(self.creator)
+
+        while funcs:
+            f = funcs.pop()
+            gys = [output().grad for output in f.ouputs]
+            gxs = f.backward(*gys)
+            if not isinstance(gxs, tuple):
+                gxs = (gxs,)
+
+            for x, gx in zip(f.inputs, gxs):
+                if x.grad is None:
+                    x.grad = gx
+                else:
+                    x.grad = x.grad + gx
+
+            if x.creator is not None:
+                add_func(x.creator)
+
+        if not retain_grad:
+            for y in f.outputs:
+                y().grad = None  # y는 약한 참조(weakref)
+
+    def cleargrad(self):
+        self.grad = None
+
+    @property
+    def shape(self):
+        return self.data.shape
+
+    @property
+    def ndim(self):
+        return self.data.ndim
+
+    @property
+    def size(self):
+        return self.data.size
+
+    @property
+    def dtype(self):
+        return self.data.dtype
+
+    def __len__(self):
+        return len(self.data)
+
+    def __repr__(self):
+        if self.data is None:
+            return 'variable(None)'
+        p = str(self.data).replace('\n', '\n' + ' ' * 9)
+        return 'variable(' + p + ')'
+
+    # def __mul__(self, other):
+    #     return mul(self, other)
+
+
+def as_variable(obj):
+    if isinstance(obj, Variable):
+        return obj
+    return Variable(obj)
+
+
+def as_array(x):
+    if np.isscalar(x):
+        return np.array(x)
+    return x
+
+
+class Function:
+    def __call__(self, *inputs):
+        inputs = [as_variable(x) for x in inputs]
+        xs = [x.data for x in inputs]
+        ys = self.forward(*xs)  # *로 언팩
+        if not isinstance(ys, tuple):  # 튜플이 아닌 경우 추가 지원
+            ys = (ys, )
+        outputs = [Variable(as_array(y)) for y in ys]
+
+        if Config.enable_backprop:
+            self.generation = max([x.generation for x in inputs])
+            for output in outputs:
+                output.set_creator(self)
+            self.inputs = inputs
+            self.outputs = [weakref.ref(output) for output in outputs]
+
+        # 리스트의 원소가 하나라면 첫 번째 원소를 반환한다
+        return outputs if len(outputs) > 1 else outputs[0]
+
+    def forward(self, x):
+        raise NotImplementedError()
+
+    def backward(self, gy):
+        raise NotImplementedError()
+
+
+class Square(Function):
+    def forward(self, x):
+        return x ** 2
+
+    def backward(self, gy):
+        x = self.inputs[0].data
+        gx = 2 * x * gy
+        return gx
+
+
+def square(x):
+    f = Square()
+    return f(x)
+
+
+class Add(Function):
+    def forward(self, x0, x1):
+        y = x0 + x1
+        return y
+
+
+def add(x0, x1):
+    x1 = as_array(x1)
+    return Add()(x0, x1)
+
+
+class Mul(Function):
+    def forward(self, x0, x1):
+        y = x0 * x1
+        return y
+
+    def backward(self, gy):
+        x0, x1 = self.inputs[0].data, self.inputs[1].data
+        return gy * x1, gy * x0
+
+
+def mul(x0, x1):
+    return Mul()(x0, x1)
+
+
+class Neg(Function):
+    def forward(self, x):
+        return -x
+
+    def backward(self, gy):
+        return -gy
+
+
+def neg(x):
+    return Neg()(x)
+
+
+class Sub(Function):
+    def forward(self, x0, x1):
+        y = x0 - x1
+        return y
+
+    def backward(self, gy):
+        return gy, -gy
+
+
+def sub(x0, x1):
+    x1 = as_array(x1)
+    return Sub()(x0, x1)
+
+
+def rsub(x0, x1):
+    x1 = as_array(x1)
+    return Sub()(x1, x0)  # x0, x1 순서 변경
+
+
+class Div(Function):
+    def forward(self, x0, x1):
+        y = x0/x1
+        return y
+
+    def backward(self, gy):
+        x0, x1 = self.inputs[0].data, self.inputs[1].data
+        gx0 = gy/x1
+        gx1 = gy * (-x0 / x1 ** 2)
+        return gx0, gx1
+
+
+def div(x0, x1):
+    x1 = as_array(x1)
+    return Div()(x0, x1)
+
+
+def rdiv(x0, x1):
+    x1 = as_array(x1)
+    return Div()(x1, x0)  # x0,x1 순서 변경
+
+
+class Pow(Function):
+    def __init__(self, c):
+        self.c = c
+
+    def forward(self, x):
+        y = x ** self.c
+        return y
+
+    def backward(self, gy):
+        x = self.inputs[0].data
+        c = self.c
+        gx = c * x ** (c-1) * gy
+        return gx
+
+
+def pow(x, c):
+    return Pow(c)(x)
+
+
+class Config:
+    enable_backprop = True
+
+
+@contextlib.contextmanager
+def using_config(name, value):
+    old_value = getattr(Config, name)
+    setattr(Config, name, value)
+    try:
+        yield
+    finally:
+        setattr(Config, name, old_value)
+
+
+def no_grad():
+    return using_config('enable_backprop', False)
+
+
+with using_config('enable_backprop', False):
+    x = Variable(np.array(2.0))
+    y = square(x)
+
+with no_grad():
+    x = Variable(np.array(2.0))
+    y = square(x)
+
+
+## example test codes ##
+x = Variable(np.array(2.0))
+y = -x  # 부호를 바꾼다
+print(y)
+
+Variable.__sub__ = sub
+Variable.__rsub__ = rsub
+
+x = Variable(np.array(2.0))
+y1 = 2.0 - x
+y2 = x - 1.0
+print(y1)
+print(y2)
+
+Variable.__truediv__ = div
+Variable.__rtruediv__ = rdiv
+Variable.__pow__ = pow
+
+x = Variable(np.array(2.0))
+y = x ** 3
+print(y)
---
+++ steps/step23.py
@@ -0,0 +1,18 @@
+
+## 패키지로 정리 ##
+# dezero 폴더 생성 후 core_simple.py 작성
+
+if '__file__' in globals():
+    import os
+    import sys
+    sys.path.append(os.path.join(os.path.dirname(__file__), '..'))
+
+import numpy as np
+from dezero import Variable
+
+x = Variable(np.array(1.0))
+y = (x + 3) ** 2
+y.backward()
+
+print(y)
+print(x.grad)
