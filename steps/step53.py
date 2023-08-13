b6c7fe4a5ac1552254ae91539b6fa52816d6ecbc
PILSUCHOI <chlvlftn22@gmail.com>
2023-08-13 11:37:00 +0900

    step53 model save and load add

--- steps/step53.py
+++ steps/step53.py
@@ -1,1 +1,45 @@
+if '__file__' in globals():
+    import os
+    import sys
+    sys.path.append(os.path.join(os.path.dirname(__file__), '..'))
+import dezero
+import dezero.functions as F
+from dezero import optimizers
+from dezero import DataLoader
+from dezero.models import MLP
+
 ## 모델 저장 및 읽어오기 ##
+
+# dezero/layers.py Layer class의 _flatten_params 메서드 추가
+# dezero/layers.py save_weight, load_weight 메서드 추가
+
+
+max_epoch = 2
+batch_size = 100
+
+train_set = dezero.datasets.MNIST(train=True)
+train_loader = DataLoader(train_set, batch_size)
+
+model = MLP((1000, 10))
+optimizer = optimizers.SGD().setup(model)
+
+# 매개변수 읽기
+if os.path.exists('my_mpl.npz'):
+    model.load_weight('my_mlp.npz')
+
+for epoch in range(max_epoch):
+    sum_loss = 0
+
+    for x, t in train_loader:
+        y = model(x)
+        loss = F.softmax_cross_entropy(y, t)
+        model.cleargrads()
+        loss.backward()
+        optimizer.update()
+        sum_loss += float(loss.data) * len(t)
+
+    print('epoch: {}, loss: {:.4f}'.format(
+        epoch + 1, sum_loss / len(train_set)))
+
+# 매개변수 저장하기
+model.save_weights('my_mpl.npz')
